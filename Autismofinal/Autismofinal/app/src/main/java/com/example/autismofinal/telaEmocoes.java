a432b8e7d2eb75bb85f7b3c2451e19ae5599605d
nataly tobias <80841883+natalytobias@users.noreply.github.com>
2024-12-23 23:27:10 -0300

    Add files via upload

---
+++ Autismofinal/Autismofinal/app/src/main/java/com/example/autismofinal/telaEmocoes.java
@@ -0,0 +1,89 @@
+package com.example.autismofinal;
+
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
+
+import java.util.Locale;
+
+
+public class telaEmocoes extends AppCompatActivity implements TextToSpeech.OnInitListener  {
+
+    private TextToSpeech textToSpeech;
+    Button btn_back;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_tela_emocoes);
+
+
+        textToSpeech = new TextToSpeech(this, this);
+
+
+        ImageView img_dor = findViewById(R.id.img_dor);
+        img_dor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speak("dor");
+            }
+        });
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btn_back = findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                finish();
+            }
+        });
+
+
+    }
+
+    // Método para falar o texto
+    private void speak(String text) {
+        if (textToSpeech != null) {
+            textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
+        }
+    }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            int langResult = textToSpeech.setLanguage(Locale.getDefault());
+            if (langResult == TextToSpeech.LANG_MISSING_DATA || langResult == TextToSpeech.LANG_NOT_SUPPORTED) {
+            }
+        } else {
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (textToSpeech != null) {
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
+        super.onDestroy();
+    }
+
+}
